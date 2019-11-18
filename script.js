e1a424858699ff18fa38586442e7d120db9664b0
Yuri-9 <vashkevichyp@gmail.com>
2019-11-19 00:38:39 +0300

    refactor: apply eslint

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
-
-let width = array32x32[0].length;
-let height = array32x32.length;
-scale = 128;
-draw4x4();
+/* eslint-disable no-plusplus */
+/* eslint-disable no-undef */
 
 function draw4x4() {
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
   for (let i = 0; i < array4x4.length; i++) {
     for (let j = 0; j < array4x4[0].length; j++) {
-      ctx.fillStyle = '#' + array4x4[i][j];
+      ctx.fillStyle = `#${array4x4[i][j]}`;
       ctx.fillRect(j * 128, i * 128, 128, 128);
     }
   }
-};
+}
+draw4x4();
 
-function draw32x32() {  
+function draw32x32() {
   const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');  
+  const ctx = canvas.getContext('2d');
   for (let i = 0; i < array32x32.length; i++) {
     for (let j = 0; j < array32x32[0].length; j++) {
-      ctx.fillStyle = ('rgba(' + array32x32[i][j] + ')'); 
+      ctx.fillStyle = (`rgba(${array32x32[i][j]})`);
       ctx.fillRect(j * 16, i * 16, 16, 16);
     }
   }
-  };
+}
 
- function draw256x256() {
+function draw256x256() {
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
-  var img = document.getElementById("img");
+  const img = document.getElementById('img');
   ctx.drawImage(img, 0, 0, 512, 512);
 }
 document.getElementById('size4x4').addEventListener('click', draw4x4);
